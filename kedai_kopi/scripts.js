d9f2ebc9ba30b98204057616ad29ddbf808ddbf3
conkeh <132237963+conkeh@users.noreply.github.com>
2024-03-26 10:32:25 -0400

    Add files via upload

--- kedai_kopi/scripts.js
+++ kedai_kopi/scripts.js
@@ -1,6 +1,6 @@
 // toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika huamburger menu di click
+// ketika hamburger menu di click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
@@ -15,9 +15,18 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+// ketika shopping cart menu di click
+document.querySelector("shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // click di luar /elemen sidebar untuk menghilangkan nav
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -27,4 +36,17 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelector(".item-detail-button");
+
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
